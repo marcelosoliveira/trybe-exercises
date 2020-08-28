c58c0cc8d55a6505a9273a953cb9ca4faf3d8af8
marcelosoliveira <msbobsk8@gmail.com>
2020-08-27 22:16:43 -0300

    Foi feito exercicios do bloco_4 dia_1 do 1 ao 5

---
+++ modulo_1/bloco_4/dia_1/exercise_1.js
@@ -0,0 +1,18 @@
+let a = 30;
+let b = 15;
+console.log("a = ", a, "b = ", b, "\n");
+
+let soma = a + b;
+console.log("Adição: ", soma, "\n");
+
+let sub = a - b;
+console.log("Subtação: ", sub, "\n");
+
+let mult = a * b;
+console.log("Multiplicação: ", mult, "\n");
+
+let div = a / b;
+console.log("Divisão: ", div, "\n");
+
+let mod = a % b;
+console.log("Módulo: ", mod, "\n");
