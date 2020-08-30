b33e807fde6e2dccc116870f37bb48fb56931647
marcelosoliveira <msbobsk8@gmail.com>
2020-08-29 21:25:58 -0300

    Adicionando exercise 8

---
+++ modulo_1/bloco_4/dia_1/exercise_8.js
@@ -0,0 +1,21 @@
+let num1 = 9;
+let num2 = 7;
+let num3 = 17;
+
+let numPar = 0;
+
+if (num1 % 2 === 0) {
+    numPar += 1;
+}
+if(num2 % 2 === 0) {
+    numPar += 1;
+}
+if(num3 % 2 === 0) {
+    numPar += 1;
+}
+
+if (numPar > 0) {
+    console.log("Tem número par? ",true);
+}else {
+    console.log("Tem número par? ", false);
+}
