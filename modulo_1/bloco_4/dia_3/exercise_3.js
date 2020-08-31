2ff4d9afb4dc35531ea642bd5e0cb7a9f4df2363
marcelosoliveira <msbobsk8@gmail.com>
2020-08-31 19:27:40 -0300

    Adicionando o exercicio 3

--- modulo_1/bloco_4/dia_3/exercise_3.js
+++ modulo_1/bloco_4/dia_3/exercise_3.js
@@ -1,10 +1,16 @@
 let n = 5;
 let aster = "";
+let position = n;
 
-for (let i = 1; i <= n; i += 1){
-    aster = "";
-    for (let j = 1; i <= j; j += 1){       
-        aster += " ";     
+for (let i = 1; i <= n; i += 1) {
+    for (let j = 1; j <= n; j += 1 ){
+        if (j < position) {
+            aster += " "; 
+        }else {
+            aster += "*";
+        }
     }
-    console.log(aster); 
+    console.log(aster);
+    aster = "";
+    position -= 1;
 }
