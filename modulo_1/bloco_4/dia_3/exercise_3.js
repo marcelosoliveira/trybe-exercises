7fedda59163e329e05219e879f36313ba9a322e5
marcelosoliveira <msbobsk8@gmail.com>
2020-08-31 18:05:57 -0300

    Exercicios 1 e 2 estão feitos, fazendo o exercicio 3

---
+++ modulo_1/bloco_4/dia_3/exercise_3.js
@@ -0,0 +1,10 @@
+let n = 5;
+let aster = "";
+
+for (let i = 1; i <= n; i += 1){
+    aster = "";
+    for (let j = 1; i <= j; j += 1){       
+        aster += " ";     
+    }
+    console.log(aster); 
+}
