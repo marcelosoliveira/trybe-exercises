59e43d5785e4cb55e32a6bd3400f464083bf987c
marcelosoliveira <msbobsk8@gmail.com>
2020-09-01 11:21:39 -0300

    todo exercises estão completos, inclusive o exercise 5

--- modulo_1/bloco_4/dia_3/exercise_5.js
+++ modulo_1/bloco_4/dia_3/exercise_5.js
@@ -1,15 +1,16 @@
 let n = 7;
 let aster = "";
 let position = n;
-let meio = " ";
-
 
 for (let i = 1; i <= n; i += 1) {
-    for (let j = 1; j <= i; j += 1) {
+    for (let j = 1; j <= i; j += 1) {          
         if(j < position) {
             aster += " ";
-        }else {
-            
+        }else if (j === 4 && i === 5) {
+            aster += " ";
+        } else if ((j > 2 && j < 6) && i === 6) {
+            aster += " ";
+        }else {            
             aster += "*";
         }      
     }
