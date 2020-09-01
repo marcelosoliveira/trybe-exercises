e7cfb98ceacff19ec905323da25fddab7ea29225
marcelosoliveira <msbobsk8@gmail.com>
2020-08-31 21:27:15 -0300

    Modificando o exercise 4 e fazendo o exercise 5

--- modulo_1/bloco_4/dia_3/exercise_5.js
+++ modulo_1/bloco_4/dia_3/exercise_5.js
@@ -1,14 +1,17 @@
-let n = 5;
+let n = 7;
 let aster = "";
 let position = n;
+let meio = " ";
+
 
 for (let i = 1; i <= n; i += 1) {
     for (let j = 1; j <= i; j += 1) {
         if(j < position) {
             aster += " ";
         }else {
-             aster += "*";
-        }    
+            
+            aster += "*";
+        }      
     }
     console.log(aster);
     aster = "";
