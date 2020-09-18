287cecadb160a4942ad6047162fa717ac8ef05ac
marcelosoliveira <msbobsk8@gmail.com>
2020-09-18 13:13:51 -0300

    Exercicio bloco 6 dia 1 terminado

--- Modulo_1-Desenvolvimento Web/bloco_6/dia_1/script.js
+++ Modulo_1-Desenvolvimento Web/bloco_6/dia_1/script.js
@@ -13,3 +13,54 @@ for (let index = 0; index < estado.length; index += 1) {
     option.innerText = estado[index];
     select.append(option);
 }
+
+let dataInput = document.querySelector('#data')
+dataInput.addEventListener('blur', (event) => {
+    let data = event.target.value
+    console.log(data);
+    let dataFormat = [];    
+        for (let index = 0; index < data.length; index += 1) {
+            dataFormat.push(data[index]);
+            console.log(index);
+            if (index === 1) {
+                dataFormat.push('/');
+        }
+        if (index === 3) {
+                dataFormat.push('/');
+        }  
+                                    
+       }
+       dataFormat1 = dataFormat.slice(0, 3);
+       dataFormat2 = dataFormat.slice(3, 6);
+       dataFormat3 = dataFormat.slice(6, 10);
+         
+   dataInput.value = `${dataFormat1.join('')}${dataFormat2.join('')}${dataFormat3.join('')}`;  
+});
+
+
+function onlynumber(evt) {
+   var theEvent = evt || window.event;
+   var key = theEvent.keyCode || theEvent.which;
+   key = String.fromCharCode( key );
+   var regex = /^[0-9.]+$/;
+   if( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+   }
+}
+ 
+let botaoEnviar = document.querySelector('#botao-enviar');
+let retornar = document.querySelector('#request-id');
+function formsRequest() {
+    let capitura = document.cadastro.nome.value + '<br>';
+    capitura += document.cadastro.email.value + '<br>';
+    capitura += document.cadastro.cpf.value + '<br>';
+    capitura += document.cadastro.endereco.value + '<br>';
+    capitura += document.cadastro.cidade.value + '<br>';
+    capitura += document.cadastro.estado.value + '<br>';
+    capitura += document.cadastro.morada.value + '<br>';
+    capitura += document.cadastro.area.value + '<br>';
+    capitura += document.cadastro.cargo.value + '<br>';
+    capitura += document.cadastro.data.value + '<br>';
+    retornar.innerHTML = capitura;
+}
