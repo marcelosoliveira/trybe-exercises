61d1fc0cb4be53708fa9f60fb7de1dd8104e4b2f
marcelosoliveira <msbobsk8@gmail.com>
2020-09-19 10:42:46 -0300

    finalizando o exercicio bloco 6 dia 2

--- Modulo_1-Desenvolvimento Web/bloco_6/dia_2/js/script.js
+++ Modulo_1-Desenvolvimento Web/bloco_6/dia_2/js/script.js
@@ -1,6 +1,6 @@
 let select = document.querySelector('#estado');
 
-let estado = ['Selecione', 'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 
+let estado = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 
 'Distrito Federal', 'Espirito Santo' , 'Goiás', 'Maranhão', 'Mato Grosso do Sul', 'Mato Grosso',
 'Minas Gerais', 'Pará', 'Paraíba', 'Parana', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 
 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Ronraima', 'Santa Catarina',
