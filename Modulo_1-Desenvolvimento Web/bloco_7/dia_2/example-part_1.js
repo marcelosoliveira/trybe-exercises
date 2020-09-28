5c0640ac487f9acbbb811abcc6b9d1a22b3a61cb
marcelosoliveira <msbobsk8@gmail.com>
2020-09-28 16:13:43 -0300

    example part 4 content

--- Modulo_1-Desenvolvimento Web/bloco_7/dia_2/example-part_1.js
+++ Modulo_1-Desenvolvimento Web/bloco_7/dia_2/example-part_1.js
@@ -1,23 +1,20 @@
 const customer = {
-    firstName: 'Roberto',
-    age: 22,
-    job: 'Teacher',
-  };
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
 
-  console.log(customer);
+console.log(customer);
+customer.lastname = 'Santos';
+console.log(customer);
 
-  customer.lastname = 'Santos';
-  console.log(customer);
-
-  const fullname = `${customer.firstName} ${customer.lastname}`;
-  let newKey = 'fullname';
-
-  customer[newKey] = fullname;
-
-  console.log(customer);
+const fullname = `${customer.firstName} ${customer.lastname}`;
+let newKey = 'fullname';
+customer[newKey] = fullname;
+console.log(customer);
 
 const name = {};
 console.log(name);
- const objectFunciton = (obj, string, value) => obj[string] = value;
+const objectFunciton = (obj, string, value) => obj[string] = value;
 objectFunciton(name, 'nome', 'Marcelo');
 console.log(name);
