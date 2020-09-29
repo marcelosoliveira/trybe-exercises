e1647dd412cd93a3b4c756e69f8435c4026554a1
marcelosoliveira <msbobsk8@gmail.com>
2020-09-28 22:32:43 -0300

    exercise 2 bonus

--- Modulo_1-Desenvolvimento Web/bloco_7/dia_2/exercise_1_2-bonus.js
+++ Modulo_1-Desenvolvimento Web/bloco_7/dia_2/exercise_1_2-bonus.js
@@ -18,7 +18,7 @@ const allLessons = {
     turno: 'noite'
   }
 }
-/* 
+/*EXERCISES BONUS 
 1.Crie uma função para contar quantos estudantes assistiram às 
 aulas de Matemática. Use o objeto criado no exercício 5. */
 console.log('Exercise 1 bonus');
@@ -33,3 +33,24 @@ const studyMath = () => {
   return `Quantidade de estudantes de Matemática é ${estudantes}`;
 }
 console.log(studyMath());
+console.log('----------------------------------');
+
+/* 2.Crie uma função que deverá retornar um objeto que representa o relatório do 
+professor ou professora, as aulas que ele ou ela ministrou e o número total de 
+estudantes. Use o objeto criado no exercício 5: */
+console.log('Exercise 2 Bonus');
+const createReport = (object, professor) => {
+  const prof = Object.values(object);
+  const newObject = {professor: '', aulas: '', estudantes: 0,};
+  let arr = [];
+  for (let index = 0; index < prof.length; index += 1) {
+    if (prof[index].professor === professor) {
+      arr.push(prof[index].materia);
+      newObject.professor = professor;
+      newObject.aulas = arr;
+      newObject.estudantes += prof[index].numeroEstudantes;
+    }
+  }
+  return newObject;
+}
+console.log(createReport(allLessons, 'Maria Clara'));
