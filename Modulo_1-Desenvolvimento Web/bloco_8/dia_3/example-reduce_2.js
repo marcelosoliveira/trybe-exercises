046265130e24d1f1d1a907203ae697d10df2dfdd
marcelosoliveira <msbobsk8@gmail.com>
2020-10-07 15:04:21 -0300

    Contúdo bloco 8 dia 3 exemplo função reduce

---
+++ Modulo_1-Desenvolvimento Web/bloco_8/dia_3/example-reduce_2.js
@@ -0,0 +1,12 @@
+const collection = [1, 2, 3, 4, 5];
+
+const getSum = (accumulator, number) => {
+  console.log(accumulator); // 1 3 6 10
+  return accumulator + number;
+};
+
+const sumNumbers = collection.reduce(getSum);
+console.log(sumNumbers); // 15
+//Ou
+const sum = collection.reduce((somatoria, number) => somatoria + number);
+console.log(sum);
