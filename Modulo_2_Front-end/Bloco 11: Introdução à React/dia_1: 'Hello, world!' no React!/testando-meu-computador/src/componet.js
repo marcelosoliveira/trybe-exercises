fe10520366774e0d7f9ff03752be58816b7cfba4
marcelosoliveira <msbobsk8@gmail.com>
2020-11-05 20:59:52 -0300

    Conteúdo hello, world ok

---
+++ Modulo_2_Front-end/Bloco 11: Introdução à React/dia_1: 'Hello, world!' no React!/testando-meu-computador/src/componet.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class TestComponet extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Marcelo dos Santos Oliveira</h1>
+        <p>Desenvolvedor FullStack Web</p>
+      </div>
+    );
+  }
+}
+
+export default TestComponet;
---
+++ Modulo_2_Front-end/Bloco 11: Introdução à React/dia_1: 'Hello, world!' no React!/testando-meu-computador/src/helloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+class ReactClass extends React.Component {
+  render() {
+    return (
+      <h1 className="hello">Hello, World!</h1>
+    )
+  }
+}
+
+export default ReactClass;
