0bdc26ed76c3052fb78b7da3365e2dff1cba791b
marcelosoliveira <msbobsk8@gmail.com>
2021-02-10 20:58:18 -0300

    bloco 20 dia 1 exercicios ok

---
+++ Modulo_3_Back-end/Bloco 20: Introdução à SQL/dia_1: Banco de dados SQL/exercicios.sql
@@ -0,0 +1,23 @@
+USE sakila;
+
+SELECT * FROM actor;
+
+/* Exercícios 1 ao 3 */
+SELECT * FROM sakila.actor;
+SELECT * FROM sakila.actor;
+SELECT * FROM sakila.inventory;
+
+/*Exercício 4 
+Faz referência a tabela country 
+
+Exercício 5
+Muitos para um
+
+Exercício 6
+Um para muitos
+
+Exercício 7
+
+address e city
+city e country
+store e address
